63f2003b7004f6c593a2a7d6c92c92653d393001
exezamar <exe.zamar@gmail.com>
2017-07-27 13:10:21 -0300

    first commit

---
+++ habitables.php
@@ -0,0 +1,222 @@
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>Containex-habitables</title>
+
+	<!-- Bootstrap CSS -->
+	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/style_responsive.css">
+	<link rel="stylesheet" href="css/slick.css">
+	<link rel="stylesheet" href="css/slick-theme.css">
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+			<![endif]-->
+		</head>
+
+		<header class="back_img_top_habitables">
+			<menu>
+				<figure class="text-center">
+					<a href="index.php"><img src="img/logo.png" alt=""></a>
+				</figure>
+				<nav class="navbar navbar-default" role="navigation">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div id="menu_top" class="collapse navbar-collapse navbar-ex1-collapse">
+							<ul class="nav navbar-nav">
+								<li><a class="link_white font_white font_yan" id="page_dry" href="dry.php?page=dry">Dry</a></li>
+								<li><a class="link_white font_white font_yan" id="page_reefers" href="reefers.php?page=reefers">Reefers</a></li>
+								<li><a class="link_white font_white font_yan" id="page_habitables" href="habitables.php?page=habitables">Habitables</a></li>
+								<li><a class="link_white font_white font_yan" id="page_genset" href="genset.php?page=genset">Gen Set</a></li>
+								<li><a class="link_white font_black font_yan" id="page_logistica" href="logistica.php?page=logistica">Logística</a></li>
+								<li><a class="link_white font_black font_yan" id="page_contactos" href="contactos.php?page=contactos">Contactos</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div>
+				</nav>
+			</menu>
+	<div class="bajada text-center col-xs-12">
+		<h3 class="font_black font_yan">Containers Habitables</h3>
+	</div>
+
+</header>
+<div class="page_habitables">
+	<div class="container">
+			<div class="col-xs-12 col-md-10 fl_none center-block text-center">
+		
+				<div class="col-md-10 col-xs-12 dry_bajada font_exo text-center center-block">
+					<b>Entre las diversas opciones que le ofrecemos, podemos mencionar:</b>
+					<p></p>
+
+					<h5 class="semibold">Exterior:</h5>
+					Proceso de pulido y pintado epoxi.
+					<p></p>
+
+					<h5 class="semibold">Interior:</h5>
+ 					Revestimiento con hojas de melanina, corlock o similar, además de aislación termoacústica,
+					con poliuretano de alta densidad.
+					<p></p>
+
+					<h5 class="semibold">Instalación eléctrica:</h5>
+					Para la misma se utilizan cables anti flama y tablero con disyuntor y se realiza sobre cable canal,
+					en base a 220 v, para iluminación interna y tomas.
+					<p></p>
+
+					<h5 class="semibold">Piso:</h5>
+					Original de madera multilaminada fenólica de 30 mm de espesor. El mismo puede ser pulido,
+					laqueado o revestido con piso de goma.
+					<p></p>
+
+					<h5 class="semibold">Aberturas:</h5>
+					Los ventanas pueden ser de acero o aluminio y con rejas soldadas para mayor seguridad.
+					Las puertas son de acero reforzado con cerraduras.
+					<p></p>
+				</div>
+			</div>
+	</div>
+	<div class="col-md-8 col-xs-12 fl_none center-block text-center">
+		<div  class="slik_blocks dry_slide">
+			<div><img class="img-responsive" src="img/habitables_slide_1.jpg" alt=""></div>
+			<div><img class="img-responsive" src="img/habitables_slide_2.jpg" alt=""></div>
+			<div><img class="img-responsive" src="img/habitables_slide_3.jpg" alt=""></div>
+		</div>		
+	</div>
+	<div class="container">
+		<div class="text-center font_exo">			
+			<b>Equipos disponibles para su alquiler y/o venta.</b>
+			<div class="font_red"><b>ENTREGAS EN TODO EL PAÍS</b></div>
+		</div>
+	</div>
+
+	<h3 id="dry_almacenamiento" class="dry_title_2 text-center font-black font_yan">Oficina o Negocio</h3>
+	<div class="habitables_back_img_1">
+	</div>
+	<div class="col-md-5 col-xs-12 dry_bajada font_exo center-block text-center">
+		Ideales para adaptar una <b>Oficina</b> o bien un <b>Negocio Comercial</b>
+		a la multiplicidad de formas, estilos y tendencias.
+	</div>
+
+	<h3 class="dry_title_2 text-center font-black font_yan">Obradores o Viviendas móviles</h3>
+	<div class="habitables_back_img_2">
+	</div>
+	<div class="col-md-5 col-xs-12 dry_bajada font_exo center-block text-center">
+		<b>Containex</b>, reforma los Containers para uso tipo <b>Viviendas Móviles</b>
+		o bien para <b>Obradores</b> para grande obras.
+	</div>
+	
+	<h3 class="dry_title_2 text-center font-black font_yan">Módulos Habitables o Vestuarios</h3>
+	<div class="habitables_back_img_3">
+	</div>
+	<div class="col-md-4 col-xs-12 dry_bajada font_exo center-block text-center">
+		También <b>Containex</b>, adapta los Containers para uso de <b>Vestuarios Deportivos</b>
+		o <b>Módulos Habitables</b> temporarios
+	</div>
+
+	<div class="container">
+		<div class="col-md-10 col-xs-12 center-block fl_none"><hr></div>
+	</div>
+	<div class="text-center center-block dry_bajada dry_links">
+				<ul class="nav nav-tabs">
+	  		<li role="presentation" class="active"><img src="img/icon_1.png" alt=""></li>
+	  			  			
+	  				<li class="link_pdf"  role="presentation"><a target="_blank" href="files/habitables.pdf">Descargar PDF</a></li>
+	  					
+	  		<li><img src="img/icon_2.png" alt=""></li>
+	  		<li role="presentation"><a href="contactos.php?page=contactos&ubic=habitables">Consultar</a></li>
+		</ul>	</div>
+</div>
+
+		<footer>
+		<hr>
+		<div class="back_img_footer">
+			<div class="container">
+				
+				<!-- Modal -->
+				<div class="body_map contain_map">
+					<div id="container_map" class="col-xs-12 col-md-5 fl_none center-block ">
+							<div id="map">								
+							</div>
+					</div>
+				</div>
+				
+				<div class="footer_slick">
+					<div id="footer_first"  class="dont_map col-md-2 col-xs-12 footer_lis">
+						<h4>Categoria de Containers</h4>
+						<div class="footer_data">
+							<ul>
+								<li><a class="font_white" href="dry.php?page=dry">Containers Dry</a></li>
+								<li><a class="font_white" href="reefers.php?page=reefers">Containers Refeers</a></li>
+								<li><a class="font_white" href="habitables.php?page=habitables">Containers Habitables</a></li>
+								<li><a class="font_white" href="genset.php?page=genset">Equipos Gen Set </a></li>
+							</ul>
+						</div>
+					</div>
+					<div id="footer_second"  class="col-md-5 col-xs-12 footer_lis">
+						<h4>Depositos, Lógistica y Almacenamiento</h4>
+						<div class="footer_data">
+							<ul class="map">
+								<li id="buenos_aires"> <a href="">Buenos Aires, (CABA), Argentina</a></li>
+								<li id="ciudad_de_cordoba"> <a href="">Ciudad de Córdoba, Argentina</a></li> 
+								<li id="ciudad_de_mendoza"> <a href="">Ciudad de Mendoza, Argentina</a></li> 
+								<li id="valparaiso_chile"> <a href="">Valparaiso, Chile</a></li> 
+								<li id="asuncion_paraguay"> <a href="">Asunción, Paraguay</a></li> 
+							</ul>
+						</div>
+					</div>
+					<div  class="dont_map col-md-3 col-xs-12 footer_lis">
+						<h4>Contactos</h4>
+						<div class="footer_data">
+							<div class="icon_watsap"></div>
+							<ul>
+								<li>+54 9 1157199236</li>
+								<li>+54 9 1137693739</li>
+								<li>+54 9 1132308787</li>
+							</ul>
+							<div class="icon_msj"></div>
+							<ul>
+								<li> <a class="font_white" href="mailto:info@containex.com.ar">info@containex.com.ar</a></li>
+								<li> <a class="font_white" href="mailto:gerencia@containex.com.ar">gerencia@containex.com.ar</a></li>
+								<li> <a class="font_white" href="mailto:operaciones@containex.com.ar">operaciones@containex.com.ar</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+			<div class="copyright">
+				<div class="container">
+					<div class="col-md-7 col-xs-12 font_exo">
+						© Copyright 2015 - www.containex.com.ar - Todos los derechos reservados
+					</div>
+					<div class="col-md-4 col-xs-12 text-right">
+						<img class="icon_brand" src="img/logo_footer.png" class="img-responsive" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<!-- jQuery -->
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<!-- Bootstrap JavaScript -->
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+	<script src="js/slick.min.js"></script>
+	<script src="js/app.js"></script>
+</body>
+</html>	
---
+++ reefers.php
@@ -0,0 +1,241 @@
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>Containex-reefers</title>
+
+	<!-- Bootstrap CSS -->
+	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/style_responsive.css">
+	<link rel="stylesheet" href="css/slick.css">
+	<link rel="stylesheet" href="css/slick-theme.css">
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+			<![endif]-->
+		</head>
+
+		<header class="back_img_top_reefers">
+			<menu>
+				<figure class="text-center">
+					<a href="index.php"><img src="img/logo.png" alt=""></a>
+				</figure>
+				<nav class="navbar navbar-default" role="navigation">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div id="menu_top" class="collapse navbar-collapse navbar-ex1-collapse">
+							<ul class="nav navbar-nav">
+								<li><a class="link_white font_white font_yan" id="page_dry" href="dry.php?page=dry">Dry</a></li>
+								<li><a class="link_white font_white font_yan" id="page_reefers" href="reefers.php?page=reefers">Reefers</a></li>
+								<li><a class="link_white font_white font_yan" id="page_habitables" href="habitables.php?page=habitables">Habitables</a></li>
+								<li><a class="link_white font_white font_yan" id="page_genset" href="genset.php?page=genset">Gen Set</a></li>
+								<li><a class="link_white font_black font_yan" id="page_logistica" href="logistica.php?page=logistica">Logística</a></li>
+								<li><a class="link_white font_black font_yan" id="page_contactos" href="contactos.php?page=contactos">Contactos</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div>
+				</nav>
+			</menu>
+	<div class="bajada text-center col-xs-12">
+		<h3 class="font_black font_yan">Containers Reefers</h3>
+	</div>
+
+</header>
+<div class="page_refeers">
+	<div class="container">
+			<div class="col-xs-12 col-md-10 fl_none center-block font_exo text-center">
+				
+				<div class="dry_bajada center-block">Los <b>Containers Reefers</b> son cámaras frigoríficas diseñadas para almacenar o transportar todo tipo de
+				mercancías que necesite frío, cadena de frío o congelado. Se adaptan y solucionan de forma rápida a las
+				necesidades de nuestros clientes.</div>
+				
+				<h3 class="dry_title text-center">Medidas Disponibles:</h3>
+
+				<div class="row dry_medidas">
+					<div class="col-md-4 col-xs-12">
+						<h1 class="col-md-3 font_blue">20’</h1>
+						<div class="col-md-9 dry_detalle ">
+							<span class="font_blue"> 6 m. de largo x 2,5 m. de ancho x 2,5 m. de alto</span>
+							<span class="font_black col-xs-12">20’- 0” Length  / 8’- 0” Width / 8’- 6” Height</span>
+						</div>
+					</div>
+					<div class="col-md-4 col-xs-12">
+						<h1 class="col-md-3 font_blue">40’</h1>
+						<div class="col-md-9 dry_detalle ">
+							<span class="font_blue"> 12 m. de largo x 2,5 m. de ancho x 2,5 m. de alto</span>
+							<span class="font_black col-xs-12">40’- 0” Length  / 8’- 0” Width / 8’- 6” Height</span>
+						</div>
+					</div>
+					<div class="col-md-4 col-xs-12">
+						<h1 class="col-md-3 font_blue">40’</h1>
+						<div class="col-md-9 dry_detalle ">
+							<span class="font_blue"> 12 m. de largo x 2,5 m. de ancho x 2,9 m. de alto</span>
+							<span class="font_black col-xs-12">40’- 0” Length  / 8’- 0” Width / 9’- 6” Height</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-10 col-xs-12 dry_bajada center-block">
+					Además, tanto los equipos en alquiler como los que están a la venta, cuentan con asistencia periódica de
+					nuestros técnicos Reefers. Son fabricados en acero inoxidable interior y exterior, aislados con 10 cm
+					de poliuretano inyectado de alta densidad. Las Unidades Reefers se encuentran en perfecto estado,
+					contando cada uno de los equipos puestos en el mercado con “Garantía Containex”,
+					ya que la empresa les realiza un tratamiento de manutención permanente.
+				</div>
+			</div>
+	</div>
+	<div class="col-md-8 col-xs-12 fl_none center-block text-center">
+		<div class="slik_blocks dry_slide">
+			<div><img class="img-responsive" src="img/refeer_slide_1.jpg" alt=""></div>
+			<div><img class="img-responsive" src="img/refeer_slide_2.jpg" alt=""></div>
+			<div><img class="img-responsive" src="img/refeer_slide_3.jpg" alt=""></div>
+		</div>		
+	</div>
+	<div class="container">
+		<div class="text-center font_exo">
+			
+			<b>Equipos disponibles para su alquiler y/o venta.</b>
+			<div class="font_red"><b>ENTREGAS EN TODO EL PAÍS</b></div>
+		</div>
+	</div>
+
+	<h3 id="dry_almacenamiento" class="dry_title_2 text-center font-black font_yan">Sistema Refrigerado</h3>
+	<div class="refeers_back_img_1">
+	</div>
+	<div class="col-md-5 col-xs-12 dry_bajada font_exo center-block text-center">
+		Los <b>Containers Reefers</b> vienen equipados con equipos de refrigeración de alta
+		tecnología que permiten el <b>Transporte y Almacenamiento de productos congelados.</b>
+	</div>
+
+	<h3 class="dry_title_2 text-center font-black font_yan">Industria Alimenticia</h3>
+	<div class="refeers_back_img_2">
+	</div>
+	<div class="col-md-5 col-xs-12 dry_bajada font_exo center-block text-center">
+			Ideales para el <b>Transporte y Almacenamiento</b> de productos
+			alimenticios tales como <b>vegetales y frutas</b>
+	</div>
+	
+	<h3 class="dry_title_2 text-center font-black font_yan">Industria Lácteas</h3>
+	<div class="refeers_back_img_3">
+	</div>
+	<div class="col-md-4 col-xs-12 dry_bajada font_exo center-block text-center">
+			Tambien permiten el <b>Transporte y Almacenamiento</b> de productos
+			de la <b>industria láctea</b>
+	</div>
+
+	<h3 class="dry_title_2 text-center font-black font_yan">Industria Frigorífica</h3>
+	<div class="refeers_back_img_4">
+	</div>
+	<div class="col-md-4 col-xs-12 dry_bajada font_exo center-block text-center">
+			Su alto sistema de refrigeración permiten el <b>Transporte y Almacenamiento</b>
+			de productos de la <b>indurtia frigorífica</b> tales como <b>carnes</b>
+	</div>
+
+
+	<div class="container">
+		<div class="col-md-10 col-xs-12 center-block fl_none"><hr></div>
+	</div>
+	<div class="text-center center-block dry_bajada dry_links">
+				<ul class="nav nav-tabs">
+	  		<li role="presentation" class="active"><img src="img/icon_1.png" alt=""></li>
+	  			  			
+	  				<li class="link_pdf"  role="presentation"><a target="_blank" href="files/reefers.pdf">Descargar PDF</a></li>
+	  					
+	  		<li><img src="img/icon_2.png" alt=""></li>
+	  		<li role="presentation"><a href="contactos.php?page=contactos&ubic=reefers">Consultar</a></li>
+		</ul>	</div>
+</div>
+
+		<footer>
+		<hr>
+		<div class="back_img_footer">
+			<div class="container">
+				
+				<!-- Modal -->
+				<div class="body_map contain_map">
+					<div id="container_map" class="col-xs-12 col-md-5 fl_none center-block ">
+							<div id="map">								
+							</div>
+					</div>
+				</div>
+				
+				<div class="footer_slick">
+					<div id="footer_first"  class="dont_map col-md-2 col-xs-12 footer_lis">
+						<h4>Categoria de Containers</h4>
+						<div class="footer_data">
+							<ul>
+								<li><a class="font_white" href="dry.php?page=dry">Containers Dry</a></li>
+								<li><a class="font_white" href="reefers.php?page=reefers">Containers Refeers</a></li>
+								<li><a class="font_white" href="habitables.php?page=habitables">Containers Habitables</a></li>
+								<li><a class="font_white" href="genset.php?page=genset">Equipos Gen Set </a></li>
+							</ul>
+						</div>
+					</div>
+					<div id="footer_second"  class="col-md-5 col-xs-12 footer_lis">
+						<h4>Depositos, Lógistica y Almacenamiento</h4>
+						<div class="footer_data">
+							<ul class="map">
+								<li id="buenos_aires"> <a href="">Buenos Aires, (CABA), Argentina</a></li>
+								<li id="ciudad_de_cordoba"> <a href="">Ciudad de Córdoba, Argentina</a></li> 
+								<li id="ciudad_de_mendoza"> <a href="">Ciudad de Mendoza, Argentina</a></li> 
+								<li id="valparaiso_chile"> <a href="">Valparaiso, Chile</a></li> 
+								<li id="asuncion_paraguay"> <a href="">Asunción, Paraguay</a></li> 
+							</ul>
+						</div>
+					</div>
+					<div  class="dont_map col-md-3 col-xs-12 footer_lis">
+						<h4>Contactos</h4>
+						<div class="footer_data">
+							<div class="icon_watsap"></div>
+							<ul>
+								<li>+54 9 1157199236</li>
+								<li>+54 9 1137693739</li>
+								<li>+54 9 1132308787</li>
+							</ul>
+							<div class="icon_msj"></div>
+							<ul>
+								<li> <a class="font_white" href="mailto:info@containex.com.ar">info@containex.com.ar</a></li>
+								<li> <a class="font_white" href="mailto:gerencia@containex.com.ar">gerencia@containex.com.ar</a></li>
+								<li> <a class="font_white" href="mailto:operaciones@containex.com.ar">operaciones@containex.com.ar</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+			<div class="copyright">
+				<div class="container">
+					<div class="col-md-7 col-xs-12 font_exo">
+						© Copyright 2015 - www.containex.com.ar - Todos los derechos reservados
+					</div>
+					<div class="col-md-4 col-xs-12 text-right">
+						<img class="icon_brand" src="img/logo_footer.png" class="img-responsive" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<!-- jQuery -->
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<!-- Bootstrap JavaScript -->
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+	<script src="js/slick.min.js"></script>
+	<script src="js/app.js"></script>
+</body>
+</html>	
